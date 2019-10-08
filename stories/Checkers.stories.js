9cdc4417b167b0bbf472665aeac334ab1b515c69
Andrew Peterson <andrew@andpeterson.com>
2019-10-08 05:16:09 -0700

    restricted pieces to 1 step diagonals

--- stories/Checkers.stories.js
+++ stories/Checkers.stories.js
@@ -11,7 +11,7 @@ export const Checkers = () => {
   return <Board size={400} />;
 };
 
-const NoPieceSelected = { x: -1, y: -1 };
+const EmptySpace = 0;
 
 class Board extends React.Component {
   state = {
@@ -25,44 +25,75 @@ class Board extends React.Component {
       [0, 2, 0, 2, 0, 2, 0, 2],
       [2, 0, 2, 0, 2, 0, 2, 0]
     ],
-    selectedPiece: NoPieceSelected
+    selectedPiece: { x: -1, y: -1 },
+    isPieceSelected: false
+  };
+
+  getBoardPiece = position => {
+    return this.state.board[position.y][position.x];
+  };
+
+  isValidKingMove = () => {
+    return true;
+  };
+
+  isValidManMove = (oldPos, newPos) => {
+    const direction = this.getBoardPiece(oldPos) == 1 ? 1 : -1; //need to figure direction somehow
+    const deltaX = newPos.x - oldPos.x;
+    const deltaY = newPos.y - oldPos.y;
+    console.log((deltaX + deltaY) % 2 == 0); //Should be even cause both directions change
+    console.log(Math.abs(deltaX) <= 1);
+    console.log(Math.abs(deltaY) <= 1);
+    if (
+      this.getBoardPiece(newPos) == 0 &&
+      (deltaX + deltaY) % 2 == 0 &&
+      Math.abs(deltaX) <= 1 &&
+      Math.abs(deltaY) <= 1
+    ) {
+      return true;
+    }
+    return false;
   };
 
   movePiece = (oldPos, newPos) => {
-    let gameBoard = this.state.board; //temp
-    console.log(
-      `old x: ${oldPos.x} y: ${oldPos.y} val: ${gameBoard[oldPos.y][oldPos.x]}`
-    );
-    console.log(
-      `new x: ${newPos.x} y: ${newPos.y} val: ${gameBoard[newPos.y][newPos.x]}`
-    );
-    gameBoard[newPos.y][newPos.x] = gameBoard[oldPos.y][oldPos.x];
-    gameBoard[oldPos.y][oldPos.x] = 0;
-    this.setState({ board: gameBoard });
-    selectedPiece = NoPieceSelected;
+    if (this.isValidManMove(oldPos, newPos)) {
+      let gameBoard = this.state.board; //temp
+      console.log(
+        `old x: ${oldPos.x} y: ${oldPos.y} val: ${
+          gameBoard[oldPos.y][oldPos.x]
+        }`
+      );
+      console.log(
+        `new x: ${newPos.x} y: ${newPos.y} val: ${
+          gameBoard[newPos.y][newPos.x]
+        }`
+      );
+      gameBoard[newPos.y][newPos.x] = gameBoard[oldPos.y][oldPos.x];
+      gameBoard[oldPos.y][oldPos.x] = EmptySpace;
+      this.setState({ board: gameBoard, isPieceSelected: false });
+    }
   };
 
   spaceSelected = (new_x, new_y) => {
     const spaceSize = this.props.size / 8;
     new_x /= spaceSize;
     new_y /= spaceSize;
-
-    if (this.state.selectedPiece != NoPieceSelected) {
+    if (this.state.isPieceSelected) {
       this.movePiece(this.state.selectedPiece, { x: new_x, y: new_y }); //bad format
     }
     console.log("Selected Space:", new_x, new_y);
   };
 
-  pieceSelected = (x, y) => {
+  pieceSelected = (pieceX, pieceY) => {
     const spaceSize = this.props.size / 8;
-    this.state.selectedPiece.x = Math.floor(x / spaceSize);
-    this.state.selectedPiece.y = Math.floor(y / spaceSize);
+    pieceX = Math.floor(pieceX / spaceSize);
+    pieceY = Math.floor(pieceY / spaceSize);
+    this.setState({
+      selectedPiece: { x: pieceX, y: pieceY },
+      isPieceSelected: true
+    });
 
-    console.log(
-      "Selected Piece:",
-      Math.floor(x / spaceSize),
-      Math.floor(y / spaceSize)
-    );
+    console.log("Selected Piece:", Math.floor(pieceX), Math.floor(pieceY));
   };
 
   render() {
@@ -160,12 +191,14 @@ class Piece extends React.Component {
   }
 }
 
+//https://www.checkershistory.com/a-glossary-of-checkers.html
 /* Questions:
   - What is the purpose of the key value in the Piece and the Space class?
   - Are we guaranteed the boards position on the screen? (Is it valid to use postion to determine piece?)
   - so that Player One takes as many turns as possible and then Player Two does the same? (From README 2)(refers to jumps?)
   - there are some unused props like key and radius? Can I modify how these fields are used?
   - Can I be guaranteed that the squares will remain squres?
+  - Can I change the value of the checker pieces? (Could use this to make white v black 1 and -1 to use that as the direction)
 
   - Refer to Git version issue as discussed in https://github.com/typicode/husky/issues/326#issuecomment-517513832 for Huskey errors (PR?)
 */
@@ -174,13 +207,15 @@ class Piece extends React.Component {
   + Be able to click on pieces
   + Store which piece was selected
   + Move piece to selected space
-  - Restrict to specific spaces to move to
+  + Restrict to specific spaces to move to
+  - Restrict Man Pieces Direction
   - Add flags for testing (example restrict where to place pieces)
   - Restrict which player is allowed to go based off turns
   - If a piece is captured it is removed from the board
   - Win condition
   - If a piece reaches the opposite side it becomes a king
   - Highlight available positions
+  - If you can take a piece you must take a piece
 */
 
 /* Notes
@@ -189,6 +224,7 @@ class Piece extends React.Component {
   - Find new way to calculate which squre/piece was selected
   - Uniform formatting
   - No Magic Numbers, including gameboard 0, 1, 2
+  - Code Comments
 */
 
 /* Stretch
